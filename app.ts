693c5624aecb618de7da29ac24e751678eda4dde
Hana Stupková <hstupkova@gmail.com>
2021-12-22 16:53:06 +0100

    tuples

--- app.ts
+++ app.ts
@@ -7,10 +7,16 @@
 };
  */
 
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Maximilian',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'], // we want this to be an array of exactly 2 elements, first being a number and second a string => change it to tuple
 };
 
 console.log(person.name);
