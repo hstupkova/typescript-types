b581e8c550c1bfeb5f1814dd1b25587eff6ede09
Hana Stupková <hstupkova@gmail.com>
2021-12-22 16:42:26 +0100

    objects and arrays

--- app.ts
+++ app.ts
@@ -1,19 +1,21 @@
-const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
-  /* if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    throw new Error('Incorrect input');
-  } */
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
+/* const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'Maximilian',
+  age: 30,
+};
+ */
+
+const person = {
+  name: 'Maximilian',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
 };
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result is: ";
+console.log(person.name);
 
-add(number1, number2, printResult, resultPhrase);
-
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()) // ERROR
+}
