741d0a8b86a43a2ce48b07849b5bdeac4c278098
Hana Stupková <hstupkova@gmail.com>
2021-12-23 11:11:38 +0100

    function types

--- app.ts
+++ app.ts
@@ -1,30 +1,23 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
 
-const combine = (
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor,
-) => {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  /* if (resultConversion === 'as-number') {
-    return +result;
-  } else {
-    return result.toString();
-  } */
-};
+function printResult(num: number): void {
+  console.log('Result: ' + num);
+}
 
-const combinedAges = combine(30, 52, 'as-number');
-console.log(combinedAges);
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
 
-const combinedStringAges = combine('30', '52', 'as-number');
-console.log(combinedStringAges);
+printResult(add(5, 12));
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+let combinedValues: (a: number, b: number) => number; // function type
+combinedValues = add; // stored a pointer to the add function
+
+console.log(combinedValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
