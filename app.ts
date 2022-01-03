adb83b44122e44fb10669c75dc13569ed3b9e9ce
Hana Stupková <hstupkova@gmail.com>
2022-01-03 17:48:40 +0100

    never type

--- app.ts
+++ app.ts
@@ -8,3 +8,12 @@ userInput = 'Max'; // valid
 if (typeof userInput === 'string') {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw {
+    message: message,
+    errorCode: code,
+  };
+}
+
+generateError('An error occurred!', 500);
