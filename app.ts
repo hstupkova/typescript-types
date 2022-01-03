7c05cf33a4f334ff082047a275303d488f1e9943
Hana Stupková <hstupkova@gmail.com>
2022-01-03 17:40:29 +0100

    unknown type

--- app.ts
+++ app.ts
@@ -1,23 +1,10 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5; // valid
+userInput = 'Max'; // valid
+
+// userName = userInput; // invalid, unknown' is not assignable to 'string'
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
-
-function printResult(num: number): void {
-  console.log('Result: ' + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-let combinedValues: (a: number, b: number) => number; // function type
-combinedValues = add; // stored a pointer to the add function
-
-console.log(combinedValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
